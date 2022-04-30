a07b06ee9e51173996162124516e5d6f0b90ea2d
fatti029 <57783721+fatti029@users.noreply.github.com>
2022-04-30 08:18:07 +0600

    Add files via upload

---
+++ phpunit/TestCode/UserTest.php
@@ -0,0 +1,15 @@
+<?php
+class UserTest extends \PHPUnit\Framework\TestCase
+{
+	public function testThatWeCanGetTheFirstName()
+	{
+		$user = new \App\Models\User;
+		$user->setFirstName('Billy ');
+		$user->setLastName('Willy');
+
+		$this->assertEquals($user->getFirstName(), 'Billy');
+		$this->assertEquals($user->getLastName(), 'Willy');
+	}
+}
+
+
